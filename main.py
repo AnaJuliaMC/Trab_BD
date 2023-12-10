bfedc15756629e0d8180403f48884c1a7f5e8498
Ana Julia <anajuliadefendi@gmail.com>
2023-12-10 17:04:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlAlchemy import Usuario, Estado, Cidade, Endereco, Destinos, Origem, ClasseVoo, Passagem, Hotel, Hospedagem, UsuarioHasHospedagem, UsuarioHasPassagem
 from datetime import date
+from sqlalchemy import or_, and_
 
 engine = create_engine('postgresql://postgres:postgres@localhost:5432/trabalho-bd')
 Session = sessionmaker(bind=engine)
@@ -212,18 +213,18 @@ usuario_has_hospedagem = [
 ]
 
 
-session.add_all(usuarios)
-session.add_all(estados)
-session.add_all(cidades)
-session.add_all(enderecos)
-session.add_all(origens)
-session.add_all(destinos)
-session.add_all(classes_voo)
-session.add_all(passagens)
-session.add_all(hoteis)
-session.add_all(usuario_has_passagem)
-session.add_all(hospedagem)
-session.add_all(usuario_has_hospedagem)
+# session.add_all(usuarios)
+# session.add_all(estados)
+# session.add_all(cidades)
+# session.add_all(enderecos)
+# session.add_all(origens)
+# session.add_all(destinos)
+# session.add_all(classes_voo)
+# session.add_all(passagens)
+# session.add_all(hoteis)
+# session.add_all(usuario_has_passagem)
+# session.add_all(hospedagem)
+# session.add_all(usuario_has_hospedagem)
 
 session.commit()
 
@@ -308,3 +309,21 @@ session.query(Hospedagem).filter(Hospedagem.idhospedagem == 1).delete()
 session.query(Endereco).filter(Endereco.idendereco == 30).delete()
 
 session.close() 
+
+
+# DEFENDI CONSULTAS
+
+# consultando hospedagens ordenando das mais baratas p as mais caras
+hospedagensordenadas = session.query(Hospedagem).order_by(Hospedagem.precos.asc()).all()
+for hospedagem in hospedagensordenadas:
+    print(f"ID: {hospedagem.idhospedagem}, Preço: {hospedagem.precos}")
+
+# consultando usuarios que foram para o hotel do estado id 5 ou 1
+resultado = session.query(Usuario, Estado).join(UsuarioHasHospedagem, Usuario.cpf == UsuarioHasHospedagem.cpf).join(Hospedagem, UsuarioHasHospedagem.idhospedagem == Hospedagem.idhospedagem).join(Hotel, Hospedagem.idhotel == Hotel.idhotel).join(Endereco, Hotel.idendereco == Endereco.idendereco).join(Cidade, Endereco.idcidade == Cidade.idcidade).join(Estado, Cidade.idestado == Estado.idestado).filter(or_(Estado.idestado == '5', Estado.idestado == '1')).all()
+for usuario, estado in resultado:
+    print(f"CPF: {usuario.cpf}, ID estado: {estado.idestado}")
+
+# usuarios que viajaram na classe 2 e escreveram a palavra "recomendo" em sua avaliação
+resultado = session.query(Usuario, ClasseVoo, UsuarioHasPassagem).join(UsuarioHasPassagem, Usuario.cpf == UsuarioHasPassagem.cpf).join(Passagem, UsuarioHasPassagem.idpassagem == Passagem.idpassagem).join(ClasseVoo, Passagem.idclassevoo == ClasseVoo.idclassevoo).filter(and_(UsuarioHasPassagem.comentario.like("%recomendo%")), ClasseVoo.idclassevoo == 2)
+for usuario, classe, comentario in resultado:
+    print(f"CPF: {usuario.cpf}, id classe voo: {classe.idclassevoo}, comentario: {comentario.comentario}")
